f9866e5e2e69d1b5d6062ff2b611dafab7597d9a
Eric Wilding <ewilding8@gmail.com>
2021-03-10 22:24:38 -0600

    Basic webpack with jsx working

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+ReactDOM.render(
+  React.createElement('div', null, `Hello React`),
+  document.getElementById('app')
+)
 
-  render() {
-    return (
-      <div className = "container">
-        <h1>MTG CARD COLLECTOR</h1>
-      </div>
-    );
-  }
-}
 
-render(<App />, document.querySelector('#root'));
+// import React, { Component } from 'react';
+// import { render } from 'react-dom';
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <div className = "container">
+//         <h1>MTG CARD COLLECTOR</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// render(<App />, document.querySelector('#root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,30 @@
 const path = require('path')
 
 module.exports = {
-  entry: path.resolve(__dirname, 'client', 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'client', 'src', 'app.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'client', 'src'),
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                "targets": "defaults" 
+              }],
+              '@babel/preset-react'
+            ]
+          }
+        }]
+      }
+    ]
   }
 }
