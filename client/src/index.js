f1bfb5ab98fea8a444e1ebe6ec816ad369c73007
Eric Wilding <ewilding8@gmail.com>
2021-03-11 00:24:15 -0600

    CSS working with webpack

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
+import './index.css'
 
 // Opt-in to Webpack hot module replacement
 if (module.hot) module.hot.accept()
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,69 @@
 const path = require('path')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  entry: path.resolve(__dirname, 'client', 'src', 'index.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    open: true,
-    clientLogLevel: 'silent',
-    port: 3000
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx|js)$/,
-        include: path.resolve(__dirname, 'client', 'src'),
-        exclude: /node_modules/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                "targets": "defaults" 
-              }],
-              '@babel/preset-react'
+module.exports = env => {
+    return {
+        mode: 'development',
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].bundle.css',
+                chunkFilename: '[id].css'
+              }),
+            new webpack.HotModuleReplacementPlugin()
+        ],
+        entry: path.resolve(__dirname, 'client', 'src', 'index.js'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
+        },
+        devServer: {
+            contentBase: path.resolve(__dirname, 'dist'),
+            open: true,
+            clientLogLevel: 'silent',
+            port: 3000,
+            historyApiFallback: true,
+            hot: true
+        },
+        module: {
+            rules: [
+            {
+                test: /\.(jsx|js)$/,
+                include: path.resolve(__dirname, 'client', 'src'),
+                exclude: /node_modules/,
+                use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                    ['@babel/preset-env', {
+                        "targets": "defaults" 
+                    }],
+                    '@babel/preset-react'
+                    ]
+                }
+                }]
+            },
+            {
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'client', 'src'),
+                exclude: /node_modules/,
+                use: [
+                  {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                      //hmr: env.NODE_ENV === 'development',
+                      publicPath: '../',
+                    }
+                  },
+                  {
+                    loader: 'css-loader',
+                    options: {
+                      importLoaders: 0 
+                    }
+                  }
+                ]
+              }
             ]
-          }
-        }]
-      }
-    ]
-  }
+        }
+    }
 }
